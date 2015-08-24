146427400d8bdbb6d289bbf3e388972fd03d57a6
Slawomir Mikulski <Slawomir.Mikulski@aviva.pl>
2015-08-24 10:38:38 +0200

    Metryka 2.4.10 - Nawigacja - Nagłówki sekcji Issue #23 (pull request)

---
+++ app/analysis/headerContent.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+    name: 'headerContent',
+    description: 'Czy na stronie s¹ nag³ówki bez treœci?',
+    run: function (page, callback) {
+        page.evaluate(function () {
+            var allHeaders = document.querySelectorAll('h1,h2,h3,h4,h5,h6').length;
+            var withoutContent = document.querySelectorAll('h1:empty,h2:empty,h3:empty,h4:empty,h5:empty,h6:empty').length;
+            return [allHeaders, withoutContent];
+        }, function (headers) {
+            callback({
+                headers: headers[0],
+                withoutContent: headers[1],
+                score: 100 - Math.round(100 * headers[1] / headers[0])
+            });
+        });
+    }
+};

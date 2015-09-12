2915e03c059dd55646de1d46df029b2e08650ff8
Bartosz Galek <bartosz.galek@allegro.pl>
2015-09-12 20:37:47 +0200

    contrast back

---
+++ app/analysis/a11y.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+    name: 'contrast',
+    run: function (page, callback) {
+        page.injectJs('../app/libs/axs_testing.js');
+        return page.evaluate(function () {
+            var axsResult = axs.Audit.run().filter(function (metric) {
+                return metric.rule.code == 'AX_COLOR_01'
+            });
+            return {
+                tags: axsResult.elements,
+                score: (axsResult.result !== 'FAIL') * 100
+            };
+        }, callback);
+    }
+};
--- app/analysis/bigFont.js
+++ app/analysis/bigFont.js
@@ -3,8 +3,11 @@
 module.exports = {
     name: 'bigFont',
     run: function (page, callback) {
-        page.evaluate(function () {
-            var href = document.querySelector('a[title*="cionka du"]').getAttribute('href');
+        return page.evaluate(function () {
+            var href = document.querySelector('a[title*="cionka du"]');
+            if (href !== null) {
+                href = href.getAttribute('href');
+            }
             return {
                 score: 100 * (href !== '') * (href !== null),
                 href: href

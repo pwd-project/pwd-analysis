6dd871d8b45953d17ac3b1abf170284ce53927de
Bartosz Galek <bartosz.galek@allegro.pl>
2015-09-14 00:30:52 +0200

    contrast undefined fix

--- app/analysis/a11y.js
+++ app/analysis/a11y.js
@@ -6,6 +6,9 @@ module.exports = {
     run: function (page, callback) {
         page.injectJs('../app/libs/axs_testing.js');
         return page.evaluate(function () {
+            if (typeof axs === 'undefined') {
+                return {"score": 0};
+            }
             var axsResult = axs.Audit.run().filter(function (metric) {
                 return metric.rule.code === 'AX_COLOR_01';
             });

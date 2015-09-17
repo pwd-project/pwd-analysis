aae12dbd204e9d5ba7efdc5a2e587e56ea3c6c4c
smikulsk <smikulsk@gmail.com>
2015-09-18 00:28:44 +0200

    PWD-26 Metryka 3.3.2 - Pomoc - Etykiety lub instrukcje

--- app/multianalysis/a11y.js
+++ app/multianalysis/a11y.js
@@ -2,7 +2,7 @@
 var axs;
 
 module.exports = {
-    names: ['contrast', 'aria'],
+    names: ['contrast', 'aria', 'labels'],
     run: function (page, callback) {
         page.injectJs('../app/libs/axs_testing.js');
         return page.evaluate(function () {
@@ -16,6 +16,10 @@ module.exports = {
                     return metric.rule.code === 'AX_ARIA_01';
                 });
 
+                var labels = axsResult.filter(function (metric) {
+                    return metric.rule.code === ' AX_TEXT_01';
+                });
+
                 var result = [];
                 result.push({
                         score: (contrast[0].result !== 'FAIL') * 100
@@ -23,6 +27,10 @@ module.exports = {
                 );
                 result.push({
                         score: (aria[0].result !== 'FAIL') * 100
+                    }
+                );
+                result.push({
+                        score: (labels[0].result !== 'FAIL') * 100
                     }
                 );
                 return result;

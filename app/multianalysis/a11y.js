8b3c189bbb42f9b42dd16d5717837a2a463aa086
Krzysztof Adamowicz <kadamowi@gmail.com>
2015-09-25 23:28:54 +0200

    Przywrocenie wersji bez tablicy

--- app/multianalysis/a11y.js
+++ app/multianalysis/a11y.js
@@ -1,3 +1,4 @@
+
 'use strict';
 var axs;
 
@@ -9,26 +10,51 @@ module.exports = {
     run: function (page, callback) {
         page.injectJs('../app/libs/axs_testing.js');
         return page.evaluate(function () {
-                var codes = ['AX_COLOR_01', 'AX_ARIA_01', 'AX_TEXT_01', 'AX_ARIA_09', 'AX_ARIA_04', 'AX_HTML_02',
-                             'AX_TEXT_03', 'AX_ARIA_02', 'AX_ARIA_03', 'AX_ARIA_08', 'AX_ARIA_10'];
-
                 var axsResult = axs.Audit.run();
 
-                var metricValues = axsResult.filter(function (metric) {
-                    return codes.includes(metric.rule.code);
+                var contrast = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_COLOR_01';
                 });
 
-                var contrast = metricValues[0];
-                var aria = metricValues[1];
-                var labels = metricValues[2];
-                var ariaRoleNotScoped = metricValues[3];
-                var badAriaAttributeValue = metricValues[4];
-                var duplicateId = metricValues[5];
-                var multipleLabelableElementsPerLabel = metricValues[6];
-                var nonExistentAriaRelatedElement = metricValues[7];
-                var requiredAriaAttributeMissing = metricValues[8];
-                var requiredOwnedAriaRoleMissing = metricValues[9];
-                var unsupportedAriaAttribute = metricValues[10];
+                var aria = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_01';
+                });
+
+                var labels = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_TEXT_01';
+                });
+
+                var ariaRoleNotScoped = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_09';
+                });
+
+                var badAriaAttributeValue = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_04';
+                });
+
+                var duplicateId = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_HTML_02';
+                });
+
+                var multipleLabelableElementsPerLabel = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_TEXT_03';
+                });
+
+                var nonExistentAriaRelatedElement = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_02';
+                });
+
+                var requiredAriaAttributeMissing = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_03';
+                });
+
+                var requiredOwnedAriaRoleMissing = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_08';
+                });
+
+                var unsupportedAriaAttribute = axsResult.filter(function (metric) {
+                    return metric.rule.code === 'AX_ARIA_10';
+                });
 
                 var result = [];
                 result.push({

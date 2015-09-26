d0c11dbb570a63a7aa045a0e796215639250614a
smikulsk <smikulsk@gmail.com>
2015-09-26 22:47:07 +0200

    Nowe metryki - WARNING

--- app/multianalysis/a11y.js
+++ app/multianalysis/a11y.js
@@ -3,10 +3,12 @@
 var axs;
 
 module.exports = {
-    names: [ 'ariaRoleNotScoped', 'badAriaAttributeValue', 'aria', 'labels',
-             'duplicateId', 'contrast', 'multipleLabelableElementsPerLabel', 'nonExistentAriaRelatedElement',
-             'requiredAriaAttributeMissing', 'requiredOwnedAriaRoleMissing',
-                'unsupportedAriaAttribute'],
+    names: [ 'ariaOnReservedElement', 'ariaOwnsDescendant', 'ariaRoleNotScoped', 'audioWithoutControls', 'badAriaAttribute',
+             'badAriaAttributeValue', 'aria', 'labels', 'duplicateId', 'focusableElementNotVisibleAndNotAriaHidden',
+             'contrast', 'mainRoleOnInappropriateElement', 'elementsWithMeaningfulBackgroundImage', 'multipleAriaOwners',
+             'multipleLabelableElementsPerLabel', 'nonExistentAriaRelatedElement', 'requiredAriaAttributeMissing',
+             'requiredOwnedAriaRoleMissing', 'unsupportedAriaAttribute'],
+
     run: function (page, callback) {
         page.injectJs('../app/libs/axs_testing.js');
         return page.evaluate(function () {
@@ -14,7 +16,8 @@ module.exports = {
 
                 var codes = ['AX_ARIA_09', 'AX_ARIA_04', 'AX_ARIA_01', 'AX_TEXT_01', 'AX_HTML_02',
                              'AX_TEXT_03', 'AX_ARIA_02', 'AX_ARIA_03', 'AX_ARIA_08', 'AX_ARIA_10',
-                             'AX_COLOR_01'];
+                             'AX_COLOR_01', 'AX_ARIA_12', 'AX_ARIA_06', 'AX_AUDIO_01', 'AX_ARIA_11',
+                             'AX_IMAGE_01', 'AX_FOCUS_01', 'AX_COLOR_01', 'AX_ARIA_05', 'AX_ARIA_07'];
 
                 var metrics= axsResult.filter(function (metric) {
                     return codes.indexOf(metric.rule.code)>=0;

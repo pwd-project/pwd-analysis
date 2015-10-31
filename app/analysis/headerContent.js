62d033db5a58b7acbc5bdedf003993303e778906
Slawomir Mikulski <smikulsk@gmail.com>
2015-11-01 00:10:00 +0100

    Poprawka w metryce headerContent - uspójnienie score w sytuacji braku nagłówków

--- app/analysis/headerContent.js
+++ app/analysis/headerContent.js
@@ -6,7 +6,7 @@ module.exports = {
         return page.evaluate(function () {
             var allHeaders = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
             if (allHeaders.length === 0) {
-                return {score: 0};
+                return {score: 100};
             }
 
             var headers = {

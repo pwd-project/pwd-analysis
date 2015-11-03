bc8d33c1152de8a7f5beea7d07baf398c9aa003b
Slawomir Mikulski <smikulsk@gmail.com>
2015-11-03 01:03:34 +0100

    Poprawka w metryce alt - dopuszczenie pustych altów i blokada niepoprawnych

--- app/analysis/alt.js
+++ app/analysis/alt.js
@@ -6,11 +6,12 @@ module.exports = {
         return page.evaluate(function () {
             var allImages = document.querySelectorAll('img').length;
             var withoutAlt = document.querySelectorAll('img:not([alt])').length;
-            var withEmptyAlt = document.querySelectorAll('img[alt=""]').length;
+            var withImproperAlt = document.querySelectorAll('img[alt=" "]').length+document.querySelectorAll('img[alt="-"]').length+document.querySelectorAll('img[alt="*"]').length;
+            var withHTMLAlt = document.querySelectorAll('img[alt*="<"]').length;
             return {
                 images: allImages,
-                withoutAlt: withEmptyAlt + withoutAlt,
-                score: (allImages === 0)?100:(100 - Math.round(100 * (withEmptyAlt + withoutAlt) / allImages))
+                withoutAlt: withEmptyAlt + withoutAlt + withHTMLAlt,
+                score: (allImages === 0)?100:(100 - Math.round(100 * (withImproperAlt + withoutAlt + withHTMLAlt) / allImages))
             };
         }, callback);
     }

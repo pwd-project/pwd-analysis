2974832f34ac40dea9aac96a975b9a6bea83e550
Krzysztof Adamowicz <kadamowi@gmail.com>
2015-11-26 22:58:49 +0100

    Wyrzucenie article i figure

--- app/analysis/formattingTags.js
+++ app/analysis/formattingTags.js
@@ -5,7 +5,7 @@ module.exports = {
     run: function (page, callback) {
         return page.evaluate(function () {
             var allTags = document.querySelectorAll('body *').length;
-            var formattingTags = document.querySelectorAll('abbr,acronym,address,article,b,basefont,bdi,bdo,big,blockquote,br,center,cite,code,del,details,dfn,dialog,em,figcaption,figure,font,hr,i,ins,kbd,mark,meter,pre,q,rt,s,samp,small,strike,strong,sub,sup,time,u,wbr,body *[align]').length;
+            var formattingTags = document.querySelectorAll('abbr,acronym,address,b,basefont,bdi,bdo,big,blockquote,br,center,cite,code,del,details,dfn,dialog,em,figcaption,font,hr,i,ins,kbd,mark,meter,pre,q,rt,s,samp,small,strike,strong,sub,sup,time,u,wbr,body *[align]').length;
             return {
                 allTags: allTags,
                 formattingTags: formattingTags,

65f62628691ca8b9e6979d9201311ff2b891bb22
Krzysztof Adamowicz <kadamowi@users.noreply.github.com>
2015-12-02 11:25:33 +0100

    Update bigFont.js

--- app/analysis/bigFont.js
+++ app/analysis/bigFont.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'bigFont',
     run: function (page, callback) {
         return page.evaluate(function () {
-            var href = document.querySelector('a[title*="cionka du"],a[title*="ksza wielko"],a[id="fontInc"],a[id="inc_font"],a[onClick*="FontSize"],a[class="big-font"]');
+            var href = document.querySelector('a[title*="cionka du"],a[title*="ksza wielko"],a[title*="kszy rozmiar"],a[id="fontInc"],a[id="inc_font"],a[onClick*="FontSize"],a[class="big-font"]');
             if (href !== null) {
                 href = href.getAttribute('href');
             }
